fd3d27b66e57d8e762d1a31ff9c0745994fba264
Sappo77 <sapposad@gmail.com>
2025-06-09 12:31:50 +0800

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,5 @@
+const tombol = document.querySelector(".pembungkus button");
+
+tombol.addEventListener("click", function() {
+  alert("Ini Merupakan Halaman Kamus Bahasa Bugis!");
+});
---
+++ main3.js
@@ -0,0 +1,16 @@
+// Ambil semua tombol "Lihat Detail"
+const tombol = document.querySelectorAll(".pembungkus2 a button")
+
+// Tambahkan event listener ke setiap tombol
+tombol.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah aksi default jika dibutuhkan
+
+    // Ambil elemen <a> di dalam button
+    const a = button.closest("a")
+    const url = a.getAttribute("href")
+
+    // Buka halaman baru
+    window.open(url, "_blank") // Membuka di tab baru
+  });
+});
